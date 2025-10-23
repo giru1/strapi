f0dcfd414799689a2d8ceaf597378a2f942b8243
Vadim <yagofarov.vadim@mail.ru>
2025-10-23 12:12:48 +0500

    edit simple cors.ts

--- config/cors.ts
+++ config/cors.ts
@@ -1,15 +1,8 @@
 export default ({ env }) => ({
     enabled: true,
-    origin: [
-        'http://localhost:1337',
-        'http://127.0.0.1:1337',
-        'http://10.1.30.17:1337',
-        'http://localhost:8000',
-        'http://127.0.0.1:8000',
-        'http://10.1.30.17:8000'
-    ],
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-    headers: ['Content-Type', 'Authorization', 'Origin', 'Accept', 'X-Requested-With'],
+    origin: ['*'], // ← временно разрешить все
+    methods: ['*'],
+    headers: ['*'],
     keepHeadersOnError: true,
     credentials: true
 });
