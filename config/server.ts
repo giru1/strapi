f8584bb0fa98323bbabdc5276bc47df75df85518
Vadim <yagofarov.vadim@mail.ru>
2025-10-29 13:08:03 +0500

    ci-cg fix start strapi 2

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'db.web.orgma.ru'),
+  url: env('PUBLIC_URL', 'http://localhost:1337'),
   app: {
     keys: env.array('APP_KEYS'),
   },
