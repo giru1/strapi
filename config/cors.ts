b578e6bf5f959edea3fc4550cd87db6ede735bfe
Vadim <yagofarov.vadim@mail.ru>
2025-11-01 17:11:13 +0500

    ci-cg fix load news 7

--- config/cors.ts
+++ config/cors.ts
@@ -3,8 +3,15 @@ export default ({ env }) => ({
     origin: [
         'https://db.web.orgma.ru',
         'http://db.web.orgma.ru',
+
         'http://localhost:1337',
-        'http://10.1.30.17:1337'
+        'http://10.1.30.17:1337',
+
+        'http://10.1.30.17:9000',
+        'http://localhost:9000',
+
+        'http://10.1.30.17:9001',
+        'http://10.1.30.17:9001'
     ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
     headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
